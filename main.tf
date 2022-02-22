aa8ff93b8a80dba1c5332a80de009fda64c2dc2f
karol <>
2022-02-22 15:14:38 +0100

    spoke2

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ module "spoke_aws_1" {
   account         = "aws-account"
   ha_gw = false
 }
+
+
+module "spoke_aws_2" {
+  source  = "terraform-aviatrix-modules/aws-spoke/aviatrix"
+  version = "4.0.3"
+  name            = "spoke-tf-cloud-2"
+  cidr            = "10.57.0.0/20"
+  region          = "us-west-1"
+  account         = "aws-account"
+  ha_gw = false
+}
